b4e9c40ac9eb01f7fddef16b1a57ccca389aacde
Olivier Friard <olivier.friard@unito.it>
2023-12-01 17:55:40 +0100

    some changes

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,39 @@
+# -- General configuration ---------------------------------------------------
+# -- General configuration
+
 extensions = [
-     'sphinx_rtd_theme',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
 ]
 
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
 
-html_theme = 'sphinx_rtd_theme'
+templates_path = ["_templates"]
 
-source_suffix = ['.rst']
+# -- Options for EPUB output
+epub_show_urls = "footnote"
 
-master_doc = 'index'
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# -- Options for HTML output -------------------------------------------------
 
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
